3f3f1def7cbe28204aa6085981e0e8279e3c9fe1
facuzavaleta <facuzavaleta890@gmail.com>
2023-12-23 23:57:40 -0300

    creado navigation y links para tasks y taskform page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,12 @@
 import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
 import {TasksPage} from './pages/TasksPage'
 import {TaskFormPage} from './pages/TaskFormPage'
-
+import {Navigation} from "./components/Navigation";
 
 function App() {
   return (
     <BrowserRouter>
+      <Navigation/>
       <Routes>
         <Route path='/' element={<Navigate to='/tasks' />} />
         <Route path='/tasks' element={<TasksPage />} />
