eae8ffec7141011e0349d9a3d5d1e16d049fdda0
facuzavaleta <facuzavaleta890@gmail.com>
2024-01-04 16:56:56 -0300

    instalado coreapi para docs

--- django_crud_api/settings.py
+++ django_crud_api/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'corsheaders',
+    'coreapi',
     'tasks',
 ]
 
@@ -127,3 +128,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = []
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
+}
