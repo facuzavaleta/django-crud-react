fe7e3ca16ac0549489365b09f9eae94143672ea3
facuzavaleta <facuzavaleta890@gmail.com>
2024-01-15 14:16:49 -0300

    creado task form, aun no funciona

--- client/src/pages/TaskFormPage.jsx
+++ client/src/pages/TaskFormPage.jsx
@@ -1,5 +1,28 @@
+import {useForm} from "react-hook-form";
+
 export function TaskFormPage() {
-    return (
-      <div>Task Form Page</div>
+  
+  const {register, handleSubmit, formState: {errors}} = useForm()
+  
+  const onSubmit = handleSubmit(data => {
+    console.log(data)
+  })
+
+  return (
+      <div>
+        <form onSubmit={onSubmit}>
+          <input type="text" placeholder="title"
+            {...register("title", {required: true})}
+          />
+          {errors.title && <span>this field is required</span>}
+
+          <textarea rows="3" placeholder="description"
+            {...register("description", {required: true})}
+          ></textarea>
+          {errors.description && <span>this field is required</span>}
+
+          <button>Save</button>
+        </form>
+      </div>
     )
   }
