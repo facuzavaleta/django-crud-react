b45b6065a7fbf451013d67c3791d4be56efa0e10
facuzavaleta <facuzavaleta890@gmail.com>
2024-01-20 23:44:52 -0300

    agregado el boton delete task, funcionando

--- client/src/pages/TaskFormPage.jsx
+++ client/src/pages/TaskFormPage.jsx
@@ -1,12 +1,14 @@
 import {useForm} from "react-hook-form";
-import {createTask} from "../api/tasks.api";
-import {useNavigate} from "react-router-dom";
+import {createTask, deleteTask} from "../api/tasks.api";
+import {useNavigate, useParams} from "react-router-dom";
 
 export function TaskFormPage() {
   
   const {register, handleSubmit, formState: {errors}} = useForm()
   
   const navigate = useNavigate()
+  const params = useParams()
+  console.log(params)
 
   const onSubmit = handleSubmit(async data => {
     const res = await createTask(data)
@@ -28,6 +30,19 @@ export function TaskFormPage() {
 
           <button>Save</button>
         </form>
+
+        {params.id && (
+          <button 
+            onClick={async () => {
+            const accepted = window.confirm('are you sure?')
+              if (accepted) {
+                await deleteTask(params.id)
+                navigate('/tasks')
+              }
+            }}>
+          Delete
+          </button>
+        )}
       </div>
     )
   }
